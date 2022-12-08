7871cd2611e2f4c1278b3455373f0885ba4fe775
AndresSN <andres.salnav@gmail.com>
2022-12-07 18:21:30 -0600

    Commit 8

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -11,7 +11,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
